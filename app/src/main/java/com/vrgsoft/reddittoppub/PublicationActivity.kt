83ddcc4d89b2dbc73dc4bb3ab6f9a4f75396f86d
Cascad <slashdrop@gmail.com>
2020-12-28 23:34:17 +0200

    Initial commit

---
+++ app/src/main/java/com/vrgsoft/reddittoppub/PublicationActivity.kt
@@ -0,0 +1,11 @@
+package com.vrgsoft.reddittoppub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publication)
+    }
+}
